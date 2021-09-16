a3d4a38c6a42217236df207020d0db2e2e67dd56
tracer-ics2021 <tracer@njuics.org>
2021-09-16 21:14:11 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: da06caf7b6f144d287c0b15f8df796f5     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  21:14:11 up  8:32,  1 user,  load average: 0.01, 0.01, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -10,7 +10,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
 	for (int i = 0; i < 32; ++ i) {
-		printf("%s 0x%08x %d\n", regs[i], cpu.gpr[i]._32, cpu.gpr[i]._32);
+		printf("%s\t0x%08x %d\n", regs[i], cpu.gpr[i]._32, cpu.gpr[i]._32);
 	}
 }
 
