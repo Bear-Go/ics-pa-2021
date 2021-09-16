59df361b521cd7f41a2d23b03df186d5f7fd5ecf
tracer-ics2021 <tracer@njuics.org>
2021-09-16 11:14:43 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: da06caf7b6f144d287c0b15f8df796f5     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  11:14:43 up 43 min,  1 user,  load average: 0.03, 0.06, 0.05

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -3,7 +3,7 @@
 NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
-	int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) || !(nemu_state.state == NEMU_QUIT);
-	printf(nemu_state.state!=NEMU_QUIT?"==\n":"<>\n");
+	int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) || (nemu_state.state == NEMU_QUIT);
+	printf(nemu_state.state==NEMU_QUIT?"==\n":"<>\n");
 	return !good;
 }
