b97d78f0233fb981c9031f12510979ac7e66fe62
tracer-ics2021 <tracer@njuics.org>
2021-09-25 16:48:47 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 8a2601d9fefc425189ba6a4d3aa1eed6     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  16:48:47 up  6:12,  1 user,  load average: 0.35, 0.26, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -88,7 +88,7 @@ static bool make_token(char *e) {
 
 				switch (rules[i].token_type) {
 					case TK_NOTYPE: break;
-					case TK_
+					//case TK_
 					default: tokens[nr_token].type = rules[i].token_type;
 							strncpy(tokens[nr_token].str, substr_start, substr_len);
 							++ nr_token;
