e78f03dc115c96bab45d80fb25d3370118838f46
tracer-ics2021 <tracer@njuics.org>
2021-09-25 19:17:21 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 8a2601d9fefc425189ba6a4d3aa1eed6     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  19:17:21 up  8:25,  1 user,  load average: 0.22, 0.14, 0.14

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -95,7 +95,7 @@ static bool make_token(char *e) {
 				break;
 			}
       	}
-		printf()
+		  
 		if (i == NR_REGEX) {
 			printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
 			return false;
