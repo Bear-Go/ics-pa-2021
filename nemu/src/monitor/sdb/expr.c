755c71dad848d3101399e7f0d349a1859dac1261
tracer-ics2021 <tracer@njuics.org>
2021-09-25 10:20:52 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 2f3a86c1ed244c5aa428af295f28e452     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  10:20:52 up 23 min,  1 user,  load average: 0.03, 0.02, 0.03

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -72,7 +72,7 @@ static bool make_token(char *e) {
 
   while (e[position] != '\0') {
     /* Try all rules one by one. */
-    f or (i = 0; i < NR_REGEX; i ++) {
+    for (i = 0; i < NR_REGEX; i ++) {
        if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
