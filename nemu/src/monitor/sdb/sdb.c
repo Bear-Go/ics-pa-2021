3b8b9fd19dc339d1b43ffcc9d0d5c31bf41f188d
tracer-ics2021 <tracer@njuics.org>
2021-09-25 17:42:19 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 8a2601d9fefc425189ba6a4d3aa1eed6     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  17:42:19 up  7:05,  1 user,  load average: 0.25, 0.12, 0.09

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -98,7 +98,7 @@ static int cmd_x(char *args) {
 	int len;
 	vaddr_t addr;
 	sscanf(arg1, "%d", &len);
-  bool *is_expr_right;
+  bool is_expr_right;
   addr = expr(arg2, &is_expr_right);
   if ( !is_expr_right ) {
       printf("Error: Wrong expression\n");
