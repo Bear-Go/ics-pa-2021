aa0cdbb5b1198e3fbf2ebc7dd3c69276a87429cd
tracer-ics2021 <tracer@njuics.org>
2021-09-26 01:28:28 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 7e063bba579b4869b2374705c3c82034     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  01:28:29 up  1:47,  1 user,  load average: 0.33, 0.33, 0.27

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -97,7 +97,7 @@ static int cmd_info(char *args) {
 }
 
 static int cmd_x(char *args) {
-  printf("%s \n",args);
+  printf("args = %s \n",args);
 	char *arg1 = strtok(NULL, " ");
   if (arg1 == NULL) {
     printf("Incomplete command\n");
@@ -105,7 +105,7 @@ static int cmd_x(char *args) {
   }
 	int len;
   sscanf(arg1, "%d", &len);
-  printf("args = %s \n", args);
+  printf("args-arg1 = %s \n", args);
 	char *arg2 = strtok(NULL, " ");
   if (arg2 == NULL) {
     printf("Incomplete command\n");
