ba7f343fd45aba664b75c90801dba1414164db3c
tracer-ics2021 <tracer@njuics.org>
2021-10-02 16:24:40 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 7e063bba579b4869b2374705c3c82034     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  16:24:40 up 19:25,  1 user,  load average: 0.28, 0.21, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -208,9 +208,9 @@ static word_t eval(int p, int q) {
 			case TK_EQ	: return val1 == val2;
 			case TK_NEQ : return val1 != val2;
 			case TK_AND : return val1 && val2;
-			case TK_NEG	: 	assert(0); 
+			case TK_NEG	: 	//assert(0); 
 							return -val2;
-			case TK_REF	: 	//assert(0);
+			case TK_REF	: 	assert(0);
 							return vaddr_read(val2, 4);
 			default: assert(0);
 		}
