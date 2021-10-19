7d915c57448be409471eb44c091e4b4919f6120f
tracer-ics2021 <tracer@njuics.org>
2021-10-19 21:15:51 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 94266757ef8044dfa3ac3ab241c7532c     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  21:15:52 up  6:11,  1 user,  load average: 0.71, 0.38, 0.38

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -5,9 +5,26 @@ def_EHelper(lui) {
 //My own code below
 
 def_EHelper(li) {
+  //wait
   rtl_li(s, ddest, id_src1->imm);
 }
 
 def_EHelper(auipc) {
+  //wait
   rtl_li(s, ddest, id_src1->imm);
+}
+
+def_EHelper(addi) {
+  //wait
+  rtl_li(s, ddest, id_src1->imm);
+}
+
+def_EHelper(jal) {
+  //wait
+  rtl_li(s, ddest, id_src1->imm);
+}
+
+def_EHelper(j) {
+  //wait
+  rtl_j(s, ddest, id_src1->imm);
 }
