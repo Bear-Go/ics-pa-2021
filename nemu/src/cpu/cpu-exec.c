b1e754528d6e66101a6be31529dc193baa3ce3d3
tracer-ics2021 <tracer@njuics.org>
2021-10-20 02:52:41 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 94266757ef8044dfa3ac3ab241c7532c     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  02:52:41 up  9:59,  1 user,  load average: 0.27, 0.28, 0.33

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -63,24 +63,24 @@ void fetch_decode(Decode *s, vaddr_t pc) {
   s->dnpc = s->snpc;
   s->EHelper = g_exec_table[idx];
 #ifdef CONFIG_ITRACE
-  // char *p = s->logbuf;
-  // p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
-  // int ilen = s->snpc - s->pc;
-  // int i;
-  // uint8_t *instr = (uint8_t *)&s->isa.instr.val;
-  // for (i = 0; i < ilen; i ++) {
-  //   p += snprintf(p, 4, " %02x", instr[i]);
-  // }
-  // int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
-  // int space_len = ilen_max - ilen;
-  // if (space_len < 0) space_len = 0;
-  // space_len = space_len * 3 + 1;
-  // memset(p, ' ', space_len);
-  // p += space_len;
+  char *p = s->logbuf;
+  p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
+  int ilen = s->snpc - s->pc;
+  int i;
+  uint8_t *instr = (uint8_t *)&s->isa.instr.val;
+  for (i = 0; i < ilen; i ++) {
+    p += snprintf(p, 4, " %02x", instr[i]);
+  }
+  int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
+  int space_len = ilen_max - ilen;
+  if (space_len < 0) space_len = 0;
+  space_len = space_len * 3 + 1;
+  memset(p, ' ', space_len);
+  p += space_len;
 
-  // void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-  // disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
-  //     MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.instr.val, ilen);
+  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+  disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
+      MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.instr.val, ilen);
 #endif
 }
 
