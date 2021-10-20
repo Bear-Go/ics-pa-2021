68cf819a184029c6ba35c119a88e2135cab3bcdf
tracer-ics2021 <tracer@njuics.org>
2021-10-20 11:32:48 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 94266757ef8044dfa3ac3ab241c7532c     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  11:32:49 up 11:59,  1 user,  load average: 0.98, 0.93, 1.30

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -41,9 +41,9 @@ typedef struct {
       uint32_t opcode1_0 : 2;
       uint32_t opcode6_2 : 5;
       uint32_t rd        : 5;
-      uint32_t imm10_1   :10;
-      uint32_t imm11     : 1;
       uint32_t imm19_12  : 8;
+      uint32_t imm11     : 1;
+      uint32_t imm10_1   :10;
       uint32_t imm20     : 1;
     } j;
     
