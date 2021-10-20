b3762ef41893b0a8ebfccdbd7fec629bd91dccfd
tracer-ics2021 <tracer@njuics.org>
2021-10-21 00:55:12 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: a7ec7a42caf741d88d6f89cd91eaead9     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  00:55:12 up  4:53,  1 user,  load average: 1.84, 1.32, 1.16

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -4,8 +4,9 @@
 #define INSTR_LIST(f) f(li) f(auipc) f(rem)\
     f(lb) f(lh) f(lw) f(lbu) f(lhu) f(sb) f(sh) f(sw)\
     f(bne) f(beq) f(bge) f(blt)\
-    f(addi) f(jal) f(ret) f(add) f(div) f(slt)\
-    f(sltu) f(xor) f(or) f(xori)\
+    f(addi) f(jal) f(ret) f(div)\
+    f(add) f(sub) f(sll) f(slt) f(sltu) f(xor) f(srl) f(sra) f(or) f(and)\
+    f(xori)\
     f(lui) f(inv) f(nemu_trap)
 
 def_all_EXEC_ID();
