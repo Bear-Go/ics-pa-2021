383e56ae8eff0874ebb44f1c65e021e4c51177fa
tracer-ics2021 <tracer@njuics.org>
2021-10-23 02:47:56 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: a31a5ac48fea47eebda9b7b11f91ea5d     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  02:47:56 up  2:44,  1 user,  load average: 0.98, 0.81, 0.67

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,7 +5,11 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  // string length
+  size_t len = 0;
+  while (*s ++) ++ len;
+  return len;
+  // panic("Not implemented");
 }
 
 char *strcpy(char *dst, const char *src) {
@@ -21,14 +25,18 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  //
-  panic("Not implemented");
+  // string cancat
+  char *ret = dst;
+  while (*dst) ++ dst;
+  while (*dst ++ = *src ++);
+  return ret;
+  // panic("Not implemented");
 }
 
 int strcmp(const char *s1, const char *s2) {
   // string compare
   int ret = 0;
-  while (!(ret = *(unsigned char*) s1 - *(unsigned char*) s2) && *s1) {++s1; ++s2;}
+  while (!(ret = *(unsigned char*) s1 - *(unsigned char*) s2) && *s1) {++ s1; ++ s2;}
   if (ret > 0) return 1;
   else if (ret < 0) return -1;
   else return 0;
@@ -40,8 +48,11 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  //
-  panic("Not implemented");
+  // memory set
+  unsigned char *p = s;
+  while (n --  > 0) *p ++ = c;
+  return s;
+  // panic("Not implemented");
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -49,11 +60,16 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  //
   panic("Not implemented");
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
+  // string compare
+  int ret = 0;
+  while (!(ret = *(unsigned char*) s1 - *(unsigned char*) s2) && *s1 && n -- > 0) {++ s1; ++ s2;}
+  if (ret > 0) return 1;
+  else if (ret < 0) return -1;
+  else return 0;
   panic("Not implemented");
 }
 
