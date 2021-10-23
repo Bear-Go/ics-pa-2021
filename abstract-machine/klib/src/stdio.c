328c59688e621d617aefd7b262b037dca8d5611b
tracer-ics2021 <tracer@njuics.org>
2021-10-24 00:33:05 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: a31a5ac48fea47eebda9b7b11f91ea5d     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  00:33:05 up 11:12,  1 user,  load average: 0.15, 0.12, 0.15

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,8 +6,52 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  assert(0);
-  return 1;
+  char out[256];
+  va_list ap;
+  va_start(ap, fmt);
+  memset(out, '\0', strlen(out));
+  char *buf = out;
+  while (*fmt) {
+    if (*fmt != '%') {
+      // regular char 
+      *buf = *fmt;
+      ++buf, ++fmt;
+      continue;
+    }
+    // format start at %
+    ++fmt;
+    switch (*fmt) {
+      case 'd': {
+        int val = va_arg(ap, int);
+        char temp[20];
+        int cnt = 0;
+        while (val >= 10) {
+          int t = val % 10;
+          temp[cnt++] = '0' + t;
+          val /= 10;
+        }
+        temp[cnt++] = '0' + val;
+        while (cnt) {
+          *buf = temp[--cnt];
+          ++ buf;
+        }
+        ++fmt;
+        break;
+      }
+      case 's': {
+        char *p = va_arg(ap, char*);
+        size_t len = strlen(p);
+        strcat(buf, p);
+        buf += len + 1;
+        ++fmt;
+        break;
+      }
+      default : assert(0);
+    }
+  }
+  size_t len = strlen(out);
+  va_end(ap);
+  return len;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
