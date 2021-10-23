6b263a11b7bf5522431c3f2c83623cdbd81dd783
tracer-ics2021 <tracer@njuics.org>
2021-10-23 14:17:53 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: a31a5ac48fea47eebda9b7b11f91ea5d     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  14:17:53 up  5:37,  1 user,  load average: 0.81, 0.78, 0.75

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -26,10 +26,6 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  // char *ret = dst;
-  // while (*dst) dst++;
-  // while ((*dst++ = *src++)) ;
-  // return ret;
   strcpy(dst + strlen(dst), src);
   return dst;
 }
@@ -44,9 +40,13 @@ char *strncat(char *dst, const char *src, size_t n) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  while (*s1 && (*s1==*s2))
-    s1++, s2++;
-  return *(const unsigned char*) s1 - *(const unsigned char*) s2;
+  for (; *s1 == *s2; ++s1, ++s2)
+    if (*s1 == 0)
+      return 0;
+  return *(unsigned char*) s1 < *(unsigned char *) s2 ? -1 : 1;
+  // while (*s1 && (*s1==*s2))
+  //   s1++, s2++;
+  // return *(const unsigned char*) s1 - *(const unsigned char*) s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
