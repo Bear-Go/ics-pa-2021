5e7cda45e8462f0afda9d01fa95c86a2b2e8c068
tracer-ics2021 <tracer@njuics.org>
2021-10-26 20:17:27 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: a7e4bc7ded2442bbb0591afc14e3eee8     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  20:17:27 up  3:05,  1 user,  load average: 0.48, 0.63, 0.61

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,7 +6,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   // uptime->us = 0;
-  uptime->us =  io_read(AM_TIMER_UPTIME).us;
+  // uptime->us =  io_read(AM_TIMER_UPTIME).us;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
