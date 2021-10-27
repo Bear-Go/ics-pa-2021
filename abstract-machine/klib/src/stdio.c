d45783be70b6722627d175c77dfe4edeed6cf978
tracer-ics2021 <tracer@njuics.org>
2021-10-27 17:26:12 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: a7e4bc7ded2442bbb0591afc14e3eee8     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  17:26:12 up  5:04,  1 user,  load average: 0.77, 0.54, 0.51

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -9,47 +9,7 @@ int printf(const char *fmt, ...) {
   char out[256];
   va_list ap;
   va_start(ap, fmt);
-  memset(out, '\0', strlen(out));
-  char *buf = out;
-  while (*fmt) {
-    if (*fmt != '%') {
-      // regular char 
-      *buf = *fmt;
-      ++buf, ++fmt;
-      continue;
-    }
-    // format start at %
-    ++fmt;
-    switch (*fmt) {
-      case 'd': {
-        int val = va_arg(ap, int);
-        char temp[20];
-        int cnt = 0;
-        while (val >= 10) {
-          int t = val % 10;
-          temp[cnt++] = '0' + t;
-          val /= 10;
-        }
-        temp[cnt++] = '0' + val;
-        while (cnt) {
-          *buf = temp[--cnt];
-          ++ buf;
-        }
-        ++fmt;
-        break;
-      }
-      case 's': {
-        char *p = va_arg(ap, char*);
-        size_t len = strlen(p);
-        strcat(buf, p);
-        buf += len + 1;
-        ++fmt;
-        break;
-      }
-      default : assert(0);
-    }
-  }
-  size_t len = strlen(out);
+  int len = vsprintf(out, fmt, ap);
   putstr(out);
   va_end(ap);
   return len;
@@ -105,47 +65,6 @@ int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   int len = vsprintf(out, fmt, ap);
-  // memset(out, '\0', strlen(out));
-  // char *buf = out;
-  // while (*fmt) {
-  //   if (*fmt != '%') {
-  //     // regular char 
-  //     *buf = *fmt;
-  //     ++buf, ++fmt;
-  //     continue;
-  //   }
-  //   // format start at %
-  //   ++fmt;
-  //   switch (*fmt) {
-  //     case 'd': {
-  //       int val = va_arg(ap, int);
-  //       char temp[20];
-  //       int cnt = 0;
-  //       while (val >= 10) {
-  //         int t = val % 10;
-  //         temp[cnt++] = '0' + t;
-  //         val /= 10;
-  //       }
-  //       temp[cnt++] = '0' + val;
-  //       while (cnt) {
-  //         *buf = temp[--cnt];
-  //         ++ buf;
-  //       }
-  //       ++fmt;
-  //       break;
-  //     }
-  //     case 's': {
-  //       char *p = va_arg(ap, char*);
-  //       size_t len = strlen(p);
-  //       strcat(buf, p);
-  //       buf += len + 1;
-  //       ++fmt;
-  //       break;
-  //     }
-  //     default : assert(0);
-  //   }
-  // }
-  // size_t len = strlen(out);
   va_end(ap);
   return len;
 }
