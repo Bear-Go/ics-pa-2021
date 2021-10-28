5374936eee931ce8bc9897ed1d242206168a9a20
tracer-ics2021 <tracer@njuics.org>
2021-10-28 19:43:52 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: c4c714d93b0143548641a700092e90aa     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  19:43:52 up  8:09,  1 user,  load average: 0.44, 0.46, 0.41

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,12 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  // int i;
-  // int w = inw(VGACTL_ADDR + 2);
-  // int h = inw(VGACTL_ADDR);
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  //  for (i = 0; i < w * h; i ++) fb[i] = i;
-  // outl(SYNC_ADDR, 1);
+  int i;
+  int w = inw(VGACTL_ADDR + 2);
+  int h = inw(VGACTL_ADDR);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+   for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -22,18 +22,24 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   // Put the image stored in pixels into window (FB_ADDR) at (x, y) of size (w * h) 
-  int fbw = inw(VGACTL_ADDR + 2);
+  int fbw = inw(VGACTL_ADDR + 2), fbh = inw(VGACTL_ADDR);
   int i, j;
   int x = ctl->x, y = ctl-> y, w = ctl->w, h = ctl->h;
   uint32_t *pixels = ctl->pixels;
   uint32_t *pen = (uint32_t *)(uintptr_t) FB_ADDR + y * fbw + x;
-  for (i = 0; i < h; ++ i) {
-    for (j = 0; j < w; ++j) {
-      *pen = *pixels;
-      pen++; pixels++;
+  if (x + w <= fbw || y + h <= fbh) {
+    for (i = 0; i < h; ++ i) {
+      for (j = 0; j < w; ++j) {
+        *pen = *pixels;
+        pen++; pixels++;
+      }
+      pen += (fbw - w);
     }
-    pen += (fbw - w);
   }
+  else {
+    printf("image too big\n");
+  }
+  // display the window
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
