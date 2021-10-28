b6efea5a3d129eef8591c58b3c035cf815241a57
tracer-ics2021 <tracer@njuics.org>
2021-10-28 15:46:02 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: c4c714d93b0143548641a700092e90aa     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  15:46:02 up  5:28,  1 user,  load average: 0.11, 0.07, 0.17

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,10 +3,13 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+#define SCREEN_W (MUXDEF(CONFIG_VGA_SIZE_800x600, 800, 400))
+#define SCREEN_H (MUXDEF(CONFIG_VGA_SIZE_800x600, 600, 300))
+
 void __am_gpu_init() {
   int i;
-  int w = 0;  // TODO: get the correct width
-  int h = 0;  // TODO: get the correct height
+  int w = SCREEN_W;  // TODO: get the correct width
+  int h = SCREEN_H;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
    for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
