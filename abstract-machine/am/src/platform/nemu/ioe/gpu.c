2d40d1493e15cf77f15c334cbdda07c3d3f72ed3
tracer-ics2021 <tracer@njuics.org>
2021-10-28 16:13:25 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: c4c714d93b0143548641a700092e90aa     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  16:13:25 up  5:40,  1 user,  load average: 0.60, 0.63, 0.42

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -23,8 +23,8 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = 100, .height = 100,
+    .vmemsz = 100
   };
 }
 
