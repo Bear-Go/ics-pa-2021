cc9b3580c21dc6911edcca1941d63e3a82845b71
tracer-ics2021 <tracer@njuics.org>
2021-10-28 17:37:06 +0800

    >  compile 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: c4c714d93b0143548641a700092e90aa     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  17:37:06 up  7:04,  1 user,  load average: 0.83, 0.65, 0.59

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -27,13 +27,14 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int i, j;
   uint32_t *pixels = ctl->pixels;
   int x = ctl->x, y = ctl-> y, w = ctl->w, h = ctl->h;
+  uint32_t *pen = (uint32_t *)(uintptr_t) FB_ADDR + y * fbw + x;
   // if (x + w >= fbw || y + h >= fbh) return;
   for (i = 0; i < w; ++ i) {
-    uint32_t *pen = (uint32_t *)(uintptr_t) FB_ADDR + (y + i) * fbw + x;
     for (j = 0; j < h; ++j) {
       *pen = *pixels;
       pen++; pixels++;
     }
+    pen += (fbw - w);
   }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
