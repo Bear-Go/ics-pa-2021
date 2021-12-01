b7487f98a60ad96648be51fae530a1de5ed3159c
tracer-ics2021 <tracer@njuics.org>
2021-12-02 01:13:58 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 59269c57c45a49258a00aff3057c3566     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  01:13:58 up 11:52,  1 user,  load average: 0.19, 0.22, 0.22

--- abstract-machine/am/include/arch/riscv32-nemu.h
+++ abstract-machine/am/include/arch/riscv32-nemu.h
@@ -3,10 +3,6 @@
 
 struct Context {
   // // TODO: fix the order of these members to match trap.S
-  // uintptr_t ra, gp, tp, t0, t1, t2, s0, s1;
-  // uintptr_t a0, a1, a2, a3, a4, a5, a6, a7;
-  // uintptr_t s2, s3, s4, s5, s6, s7, s8, s9;
-  // uintptr_t s10,s11,t3, t4, t5, t6;
   uintptr_t gpr[32], mcause, mstatus, mepc;
   void *pdir;
 };
