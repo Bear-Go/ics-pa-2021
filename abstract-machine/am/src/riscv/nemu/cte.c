0df3fe223e34af6025b55f2dc296dfe50b40f4ee
tracer-ics2021 <tracer@njuics.org>
2021-12-02 00:56:52 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 59269c57c45a49258a00aff3057c3566     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  00:56:52 up 11:35,  1 user,  load average: 0.30, 0.33, 0.27

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -6,7 +6,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
   printf("Running ## __am_irq_handle() ##\n...\n");
-  printf("(1) %u\n", c->mcause);
+  printf("(1) %08x\n", c->mcause);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
