c3e4a1f6a6f01464934f538a3a0aa2758d12711e
tracer-ics2021 <tracer@njuics.org>
2021-12-02 00:11:05 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 59269c57c45a49258a00aff3057c3566     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  00:11:05 up 10:50,  1 user,  load average: 0.41, 0.24, 0.18

--- abstract-machine/am/include/arch/riscv32-nemu.h
+++ abstract-machine/am/include/arch/riscv32-nemu.h
@@ -2,15 +2,20 @@
 #define ARCH_H__
 
 struct Context {
-  // TODO: fix the order of these members to match trap.S
-  uintptr_t mcause, mstatus, mepc, gpr[32];
+  // // TODO: fix the order of these members to match trap.S
+  uintptr_t ra, gp, tp, t0, t1, t2, s0, s1;
+  uintptr_t a0, a1, a2, a3, a4, a5, a6, a7;
+  uintptr_t s2, s3, s4, s5, s6, s7, s8, s9;
+  uintptr_t s10,s11,t3, t4, t5, t6;
+  uintptr_t mcause, mstatus, mepc;
   void *pdir;
 };
 
-#define GPR1 gpr[17] // a7
-#define GPR2 gpr[0]
-#define GPR3 gpr[0]
-#define GPR4 gpr[0]
-#define GPRx gpr[0]
+// #define GPR1 gpr[17] // a7
+#define GPR1 a7
+// #define GPR2 gpr[0]
+// #define GPR3 gpr[0]
+// #define GPR4 gpr[0]
+// #define GPRx gpr[0]
 
 #endif
