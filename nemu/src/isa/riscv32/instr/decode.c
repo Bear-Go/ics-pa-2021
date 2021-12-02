f45a45b96c85d30cafc7372897bfe9eaea5addaf
tracer-ics2021 <tracer@njuics.org>
2021-12-02 16:52:06 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 59269c57c45a49258a00aff3057c3566     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:52:06 up 18:49,  1 user,  load average: 0.92, 0.53, 0.41

--- nemu/src/isa/riscv32/instr/decode.c
+++ nemu/src/isa/riscv32/instr/decode.c
@@ -62,7 +62,7 @@ static def_DHelper(S) {
 }
 
 static def_DHelper(csr) {
-  decode_op_r(s, id_src1, s->isa.instr.csr.rs1, false);
+  decode_op_r(s, id_src1, s->isa.instr.csr.rs1, true);
   decode_op_i(s, id_src2, s->isa.instr.csr.csr, true);
   decode_op_r(s, id_dest, s->isa.instr.csr.rd, true);
 }
