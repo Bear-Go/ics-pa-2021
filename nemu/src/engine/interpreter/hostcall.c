0f38c3aebb7950b37d3970a8a9c1ad376bc14e9b
tracer-ics2021 <tracer@njuics.org>
2021-12-02 18:32:56 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 792e52c78e034a21a14204d4a2a1d3c3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  18:32:56 up 25 min,  1 user,  load average: 0.58, 0.49, 0.38

--- nemu/src/engine/interpreter/hostcall.c
+++ nemu/src/engine/interpreter/hostcall.c
@@ -85,8 +85,7 @@ static void isa_hostcall(uint32_t id, rtlreg_t *dest, const rtlreg_t *src, uint3
   switch (id) {
     case HOSTCALL_CSRRW: csrrw(dest, src, imm); break;
     case HOSTCALL_CSRRS: csrrs(dest, src, imm); break;
-    case HOSTCALL_TRAP: /* printf("Running ## isa_hostcall() HOSTCALL_TRAP ##\n...\n"); */
-      ret = isa_raise_intr(imm, *src); if (dest) *dest = ret; break;
+    case HOSTCALL_TRAP: ret = isa_raise_intr(imm, *src); if (dest) *dest = ret; break;
     case HOSTCALL_MRET: mret(dest); printf("t0 0x%08x\n", *dest); break;
     default: panic("Unsupport hostcall ID = %d", id); break;
   }
