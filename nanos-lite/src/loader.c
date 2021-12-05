2356bd52cd4f32969612cb5bb6e653508c8ad592
tracer-ics2021 <tracer@njuics.org>
2021-12-05 10:06:15 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 89c39991e9bf47c9b7dec9fc1842fc03     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  10:06:15 up 23:05,  1 user,  load average: 0.32, 0.38, 0.32

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -17,7 +17,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   size_t ramdisk_size = get_ramdisk_size();
   Elf_Ehdr *elf_ehdr = malloc(ramdisk_size);
   ramdisk_read(elf_ehdr, 0, ramdisk_size);
-  assert(*(uint32_t *)elf_ehdr->e_ident == 0xBadC0de);
+  assert(*(uint32_t *)elf_ehdr->e_ident == 0x7f454c46);
   // Elf_Phdr *elf_phdr = NULL;
   panic("here");
   panic("here");
