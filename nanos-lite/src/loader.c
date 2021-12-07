b6a5db5906d2c4dbaaa49dd7c2f5f054a2917dcd
tracer-ics2021 <tracer@njuics.org>
2021-12-07 18:23:10 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 89c39991e9bf47c9b7dec9fc1842fc03     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  18:23:10 up 1 day, 15:19,  1 user,  load average: 0.42, 0.67, 0.56

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -20,13 +20,13 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(elf_ehdr, 0, sizeof(Elf_Ehdr));
   assert(*(uint32_t *)elf_ehdr->e_ident == 0x464c457f);
   int cnt = 0;
+  Elf_Phdr *elf_phdr = (Elf_Phdr *)(base + elf_ehdr->e_phoff);
   for (int i = 0; i < elf_ehdr->e_phnum; ++i) {
-
-    Elf_Phdr *elf_phdr = (Elf_Phdr *)(base+elf_ehdr->e_phoff+i*sizeof(Elf_Phdr));
     if (elf_phdr->p_type == PT_LOAD) {
       memcpy((void *)elf_phdr->p_vaddr, (void *)base + elf_phdr->p_offset, elf_phdr->p_filesz);
       memset((void *)elf_phdr->p_vaddr + elf_phdr->p_filesz, 0, elf_phdr->p_memsz - elf_phdr->p_filesz);
       ++cnt;
+      elf_phdr += sizeof(Elf_Phdr);
       printf("1\n");
     }
   }
