07f87bbef6aa097e59f163c2e083352913fe321b
tracer-ics2021 <tracer@njuics.org>
2021-12-14 23:58:43 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:58:43 up  3:47,  1 user,  load average: 0.81, 0.55, 0.48

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -2,9 +2,26 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+const char *stdregs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+void std_reg_display() {
+	for (int i = 0; i < 32; ++ i) {
+		printf("%3s: 0x%08x ", stdregs[i], gpr(i));
+    if (i % 4 == 3) printf("\n");
+	}
+}
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0; i < 32; ++ i) {
-    if (!difftest_check_reg(reg_name(i, sizeof(word_t)), pc, ref_r->gpr[i]._32, gpr(i))) return false;
+    if (!difftest_check_reg(reg_name(i, sizeof(word_t)), pc, ref_r->gpr[i]._32, gpr(i))) {
+      std_reg_display();
+      return false;
+    }
   }
   return true;
 }
