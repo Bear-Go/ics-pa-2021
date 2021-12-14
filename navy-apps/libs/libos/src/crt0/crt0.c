629646753250f44b74120b0c36c4799a6e3d41f4
tracer-ics2021 <tracer@njuics.org>
2021-12-14 19:22:30 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  19:22:30 up  1:39,  1 user,  load average: 0.15, 0.25, 0.28

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  char *empty[] =  {NULL };
+  char *empty[] =  {NULL};
   environ = empty;
   exit(main(0, empty, empty));
   assert(0);
