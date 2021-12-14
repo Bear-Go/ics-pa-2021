481a2b449afb77069409e79360968b9c0572d768
tracer-ics2021 <tracer@njuics.org>
2021-12-15 01:43:23 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  01:43:23 up  5:32,  1 user,  load average: 0.32, 0.37, 0.36

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,13 +15,13 @@ size_t get_ramdisk_size();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
 
-  Elf_Ehdr *ehdr = NULL;
-  ramdisk_read(ehdr, 0, sizeof(Elf_Ehdr));
-  assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
 
-  for (int i = 0; i < ehdr->e_phnum; ++ i) {
+  for (int i = 0; i < ehdr.e_phnum; ++ i) {
     Elf_Phdr phdr;
-    ramdisk_read(&phdr, ehdr->e_phoff + ehdr->e_phentsize * i, sizeof(Elf_Phdr));
+    ramdisk_read(&phdr, ehdr.e_phoff + ehdr.e_phentsize * i, sizeof(Elf_Phdr));
     if (phdr.p_type == PT_LOAD) {
       ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
       memset((void *)phdr.p_vaddr + phdr.p_filesz, 0, phdr.p_memsz - phdr.p_filesz);
@@ -30,7 +30,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   // panic("##!! here !!##");
 
-  return ehdr->e_entry;
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
