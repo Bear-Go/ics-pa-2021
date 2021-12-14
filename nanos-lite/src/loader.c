5d635e02aac640263611f2a4e383c3eefc62771c
tracer-ics2021 <tracer@njuics.org>
2021-12-14 18:45:44 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  18:45:44 up  1:03,  1 user,  load average: 0.05, 0.14, 0.25

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -31,7 +31,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       memset((void *)phdr[i].p_vaddr, 0, phdr[i].p_memsz - phdr[i].p_filesz);
     } 
   }
-  panic("##!! here !!##");
+  // panic("##!! here !!##");
   return ehdr->e_entry;
 }
 
