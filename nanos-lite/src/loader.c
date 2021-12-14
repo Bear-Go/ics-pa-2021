d13d49c7a76302eef694e749744fa6e1ee0118df
tracer-ics2021 <tracer@njuics.org>
2021-12-15 01:26:53 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  01:26:53 up  5:16,  1 user,  load average: 0.47, 0.44, 0.35

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,26 +15,27 @@ size_t get_ramdisk_size();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
 
-  Elf_Ehdr *ehdr = (Elf_Ehdr *)malloc(sizeof(Elf_Ehdr));
-  ramdisk_read(ehdr, 0, sizeof(Elf_Ehdr));
-  assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
 
-  assert(ehdr->e_phentsize * ehdr->e_phnum == sizeof(Elf_Phdr) * ehdr->e_phnum);
+  Elf_Phdr *phdr = (Elf_Phdr *)malloc(ehdr.e_phentsize * ehdr.e_phnum);
+  ramdisk_read(phdr, ehdr.e_phoff, ehdr.e_phentsize * ehdr.e_phnum);
 
-  Elf_Phdr *phdr = (Elf_Phdr *)malloc(ehdr->e_phentsize * ehdr->e_phnum);
-  ramdisk_read(phdr, ehdr->e_phoff, ehdr->e_phentsize * ehdr->e_phnum);
-  for (int i = 0; i < ehdr->e_phnum; ++ i) {
+  for (int i = 0; i < ehdr.e_phnum; ++ i) {
+
     if (phdr[i].p_type == PT_LOAD) {
       void *buf = malloc(phdr[i].p_filesz);
       ramdisk_read(buf, phdr[i].p_offset, phdr[i].p_filesz);
       memcpy((void *)phdr[i].p_vaddr, buf, phdr[i].p_filesz);
       memset((void *)phdr[i].p_vaddr, 0, phdr[i].p_memsz - phdr[i].p_filesz);
     } 
+
   }
 
   // panic("##!! here !!##");
 
-  return ehdr->e_entry;
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
