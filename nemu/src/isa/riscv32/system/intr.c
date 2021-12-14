b024c3d01a70c03eecc3a377f1191e2a43b8fe5a
tracer-ics2021 <tracer@njuics.org>
2021-12-14 20:36:08 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  20:36:08 up  2:53,  1 user,  load average: 0.45, 0.47, 0.42

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -5,6 +5,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
   cpu.mcause = NO;
+  cpu.mstatus = 0x00001800;
   cpu.mepc = epc;
   return cpu.mtvec;
 }
