db7745cdbc7d5b2c0d6f9508903d6c486be11616
tracer-ics2021 <tracer@njuics.org>
2021-12-15 00:49:35 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  00:49:35 up  4:38,  1 user,  load average: 0.26, 0.24, 0.31

--- nemu/src/engine/interpreter/hostcall.c
+++ nemu/src/engine/interpreter/hostcall.c
@@ -63,7 +63,7 @@ static void csrrs(rtlreg_t *dest, const rtlreg_t *src, uint32_t csrid) {
 
 static void mret(rtlreg_t *dest) {
   word_t pc = *csr_decode(0x341);
-  *dest = pc + 2;
+  *dest = pc + 4;
 }
 
 static void isa_hostcall(uint32_t id, rtlreg_t *dest, const rtlreg_t *src, uint32_t imm) {
