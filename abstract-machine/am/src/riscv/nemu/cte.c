bc893a0bdee0549309d5525837b1c8c8c2c38f91
tracer-ics2021 <tracer@njuics.org>
2021-12-16 01:41:48 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  01:41:48 up 17:32,  1 user,  load average: 0.65, 0.40, 0.31

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -4,7 +4,22 @@
 
 static Context* (*user_handler)(Event, Context*) = NULL;
 
+const char *tregs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
 Context* __am_irq_handle(Context *c) {
+
+  printf("%8s: 0x%08x\n", "mcause", c->mcause);
+  printf("%8s: 0x%08x\n", "mepc", c->mepc);
+  printf("%8s: 0x%08x\n", "mstatus", c->mstatus);
+  for (int i = 0; i < 32; ++ i) {
+		printf("%3s: 0x%08x ", tregs[i], c->gpr[i]);
+    if (i % 4 == 3) printf("\n");
+	}
 
   if (user_handler) {
     Event ev = {0};
