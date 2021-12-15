cc973b455ad6ce91630d9bf3cd76d9013a3d1c29
tracer-ics2021 <tracer@njuics.org>
2021-12-15 14:55:29 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:55:29 up  9:35,  1 user,  load average: 0.64, 0.40, 0.29

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,22 +3,7 @@
 
 extern void do_syscall(Context*);
 
-const char *tregs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
-
 static Context* do_event(Event e, Context* c) {
-
-  printf("%8s: 0x%08x\n", "mcause", c->mcause);
-  printf("%8s: 0x%08x\n", "mepc", c->mepc);
-  printf("%8s: 0x%08x\n", "mstatus", c->mstatus);
-	for (int i = 0; i < 32; ++ i) {
-		printf("%3s: 0x%08x ", tregs[i], c->gpr[i]);
-    if (i % 4 == 3) printf("\n");
-	}
 
   switch (e.event) {
     case EVENT_YIELD: Log("event: yield"); break;
