94c8cd81497ec3ed3510e1eb416ccca49638b089
tracer-ics2021 <tracer@njuics.org>
2021-12-15 13:22:41 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  13:22:41 up  8:24,  1 user,  load average: 0.72, 0.53, 0.47

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,8 +4,10 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
   switch (a[0]) {
+    case SYS_exit:
+      halt(a[1]);
     case SYS_yield: 
       yield();
       c->GPRx = 0;
