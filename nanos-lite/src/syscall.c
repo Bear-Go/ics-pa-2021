c8e5d6ae3e5958afddef5c758d3d8bbbcdaf9cdb
tracer-ics2021 <tracer@njuics.org>
2021-12-15 15:02:45 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:02:46 up  9:43,  1 user,  load average: 0.11, 0.20, 0.23

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -28,7 +28,7 @@ void do_syscall(Context *c) {
     //   break;
     case SYS_yield: 
       printf("yield\n");
-      asm volatile("ecall");
+      yield();
       c->GPRx = 0;
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
