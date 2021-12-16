6fa1fb1fece22a8547bb98885c7af6c42d82ee76
tracer-ics2021 <tracer@njuics.org>
2021-12-16 17:15:10 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: 23029dc3d45d42f18d2f6d9cea6e2a0b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  17:15:10 up 21:56,  1 user,  load average: 0.83, 0.68, 0.51

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,22 +11,16 @@ void do_syscall(Context *c) {
 
   switch (a[0]) {
     case SYS_exit:
-      printf("exit!\n");
+      // printf("exit!\n");
       halt(a[1]);
       break;
     case SYS_yield: 
-      printf("yield!\n");
+      // printf("yield!\n");
       // if yield here then mstatus will get diff right = 0x000218000 wrong = 0x00001800
       // yield(); 
       c->GPRx = 0;
       break;
     case SYS_write:
-      putch('!');
-      printf("write!\n");
-      printf("a[0]%d\n", a[0]);
-      printf("a[1]%d\n", a[1]);
-      printf("a[2]%d\n", a[2]);
-      printf("a[3]%d\n", a[3]);
       if (a[1] == 1 || a[1] ==  2) {
         for (int i = 0; i < a[3]; ++ i) {
             putch(((char*)a[2])[i]);
