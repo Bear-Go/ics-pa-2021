aa7a64a2f7f4241e0e9898298c7623d6be2dbd87
tracer-ics2021 <tracer@njuics.org>
2022-01-10 18:38:01 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  18:38:01 up  5:40,  1 user,  load average: 0.62, 0.41, 0.38

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -29,13 +29,15 @@ void do_syscall(Context *c) {
       c->GPRx = fs_read(a[1], (void*)a[2], a[3]);
       break;
     case SYS_write:
-      if (a[1] == 1 || a[1] ==  2) {
-        for (int i = 0; i < a[3]; ++ i) {
-            putch(((char*)a[2])[i]);
-        }
-      }
+      // if (a[1] == 1 || a[1] ==  2) {
+      //   for (int i = 0; i < a[3]; ++ i) {
+      //       putch(((char*)a[2])[i]);
+      //   }
+      // }
       c->GPRx = fs_write(a[1], (const void*)a[2], a[3]);
       break;
+    case SYS_close:
+      c->GPRx = fs_close(a[1]);
     case SYS_lseek:
       c->GPRx = fs_lseek(a[1], a[2], a[3]);
       break;
