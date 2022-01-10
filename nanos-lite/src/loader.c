04bbcd2316dcfedea768fcb974c3234a9c5ef67c
tracer-ics2021 <tracer@njuics.org>
2022-01-10 17:20:36 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  17:20:36 up  4:23,  1 user,  load average: 0.38, 0.42, 0.39

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -22,7 +22,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   fs_lseek(fd, 0, SEEK_SET);
   fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
-  printf("%d", ehdr.e_phnum);
+
   for (int i = 0; i < ehdr.e_phnum; ++ i) {
     Elf_Phdr phdr;
     fs_lseek(fd, ehdr.e_phoff + ehdr.e_phentsize * i, SEEK_SET);
@@ -31,9 +31,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       fs_lseek(fd, phdr.p_offset, SEEK_SET);
       fs_read(fd, (void *)phdr.p_vaddr, phdr.p_filesz);
       memset((void *)phdr.p_vaddr + phdr.p_filesz, 0, phdr.p_memsz - phdr.p_filesz);
-      panic("here");
     } 
   }
+    panic("here");
   fs_close(fd);
 
   return ehdr.e_entry;
