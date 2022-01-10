90ecd19888f5875277a399c29b1ccff1393618df
tracer-ics2021 <tracer@njuics.org>
2022-01-10 16:27:37 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  16:27:37 up  3:30,  1 user,  load average: 0.58, 0.47, 0.39

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -10,6 +10,7 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -28,9 +29,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
@@ -48,7 +49,8 @@ int fs_open(const char* pathname, int flags, int mode) {
 }
 
 size_t fs_read(int fd, void* buf, size_t len) {
-  ramdisk_read(buf, file_table[fd].disk_offset, len);
+  size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
+  ramdisk_read(buf, offset, len);
   return len;
 }
 
@@ -57,7 +59,16 @@ size_t fs_write(int fd, const void* buf, size_t len) {
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
-  return 0;
+  if (whence == SEEK_SET) {
+    file_table[fd].open_offset = offset;
+  }
+  else if (whence == SEEK_CUR) {
+    file_table[fd].open_offset += offset;
+  }
+  else if (whence == SEEK_END) {
+    file_table[fd].open_offset += file_table[fd].disk_offset + offset;
+  }
+  panic("Wrong whence in fs_lseek: %d", whence);
 }
 
 int fs_close(int fd) {
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -11,7 +12,9 @@
 
 int fs_open(const char* pathname, int flags, int mode);
 size_t fs_read(int fd, void* buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
+
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
@@ -20,11 +23,13 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   Elf_Ehdr ehdr;
   int fd = fs_open(filename, 0, 0);
+  fs_lseek(fd, 0, SEEK_SET);
   fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
-
+  panic("here");
   for (int i = 0; i < ehdr.e_phnum; ++ i) {
     Elf_Phdr phdr;
+    fs_read(fd, &phdr, sizeof(Elf_Phdr));
     ramdisk_read(&phdr, ehdr.e_phoff + ehdr.e_phentsize * i, sizeof(Elf_Phdr));
     if (phdr.p_type == PT_LOAD) {
       ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
