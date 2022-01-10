7398b8c9edbd6fdc42b5ab9bce8d0b5696a16b7d
tracer-ics2021 <tracer@njuics.org>
2022-01-10 17:18:09 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  17:18:09 up  4:21,  1 user,  load average: 0.33, 0.49, 0.41

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,7 +16,7 @@ size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  panic("filename = %s", filename);
+  // panic("filename = %s", filename);
   Elf_Ehdr ehdr;
   int fd = fs_open(filename, 0, 0);
   fs_lseek(fd, 0, SEEK_SET);
@@ -34,6 +34,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     } 
   }
   fs_close(fd);
+  panic("finish load");
   return ehdr.e_entry;
 }
 
