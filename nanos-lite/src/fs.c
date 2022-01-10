492056f6ccb0f5dd1f388434a70ad40ebf1bebe9
tracer-ics2021 <tracer@njuics.org>
2022-01-10 21:24:21 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  21:24:21 up  8:27,  1 user,  load average: 0.67, 0.40, 0.24

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,25 +58,25 @@ size_t fs_read(int fd, void* buf, size_t len) {
   }
 
   size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
-  // if (!file_table[fd].read) {
-    ramdisk_read(buf, offset, len);
-  // }
-  // else {
-  //   len = file_table[fd].read(buf, offset, len);
-  // }
+  ramdisk_read(buf, offset, len);
   file_table[fd].open_offset += len;
   return len;
 }
 
-// size_t fs_write(int fd, const void* buf, size_t len) {
-//   // assert(file_table[fd].open_offset <= file_table[fd].size);
-//   assert(fd >= 3);
-//   if (file_table[fd].open_offset + len > file_table[fd].size) 
-//     len = file_table[fd].size - file_table[fd].open_offset;
-//   size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
-//   ramdisk_write(buf, offset, len);
-//   return len;
-// }
+size_t fs_write(int fd, const void* buf, size_t len) {
+  assert(fd >= 3);
+  if (file_table[fd].open_offset > file_table[fd].size) {
+    len = 0;
+  }
+  else if (file_table[fd].open_offset + len >= file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
+
+  size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
+  ramdisk_write(buf, offset, len);
+  file_table[fd].open_offset += len;
+  return len;
+}
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
   switch (whence) {
@@ -101,37 +101,20 @@ int fs_close(int fd) {
   return 0;
 }
 
-// size_t fs_read(int fd, void *buf, size_t len){
-//     if(fd>=3 &&(file_table[fd].open_offset+len >= file_table[fd].size)){
+// size_t fs_write(int fd, const void *buf, size_t len){
+//     if(fd>=5 &&(file_table[fd].open_offset+len > file_table[fd].size)){
 //         if(file_table[fd].size > file_table[fd].open_offset)
 //             len = file_table[fd].size - file_table[fd].open_offset;
 //         else
 //             len = 0;
 //     }
-//     if(!file_table[fd].read){
-//         ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+//     if(!file_table[fd].write){
+//         ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 //     }
 //     else{
-//         len = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+//         file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
 //     }
 //     file_table[fd].open_offset += len;
 //     return len;
 // }
-
-size_t fs_write(int fd, const void *buf, size_t len){
-    if(fd>=5 &&(file_table[fd].open_offset+len > file_table[fd].size)){
-        if(file_table[fd].size > file_table[fd].open_offset)
-            len = file_table[fd].size - file_table[fd].open_offset;
-        else
-            len = 0;
-    }
-    if(!file_table[fd].write){
-        ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-    }
-    else{
-        file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-    }
-    file_table[fd].open_offset += len;
-    return len;
-}
 
