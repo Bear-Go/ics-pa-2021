87be1bd892becd9dd3b0d0469d63b3c1c8afe33a
tracer-ics2021 <tracer@njuics.org>
2022-01-10 15:32:27 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  15:32:27 up  2:35,  1 user,  load average: 0.41, 0.35, 0.23

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -31,6 +31,28 @@ static Finfo file_table[] __attribute__((used)) = {
 #include "files.h"
 };
 
+static int file_num;
 void init_fs() {
-  // TODO: initialize the size of /dev/fb
+  Log("Initializing file system...");
+  file_num = sizeof(file_table) / sizeof(file_table[0]);
+}
+
+int fs_open(const char* pathname, int flags, int mode) {
+  return 0;
+}
+
+size_t fs_read(int fd, void* buf, size_t len) {
+  return 0;
+}
+
+size_t fs_write(int fd, const void* buf, size_t len) {
+  return 0;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  return 0;
+}
+
+int fs_close(int fd) {
+  return 0;
 }
