f447ace541903ea093f7dec9e890c30270f1941a
tracer-ics2021 <tracer@njuics.org>
2022-01-10 21:38:05 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  21:38:05 up  8:41,  1 user,  load average: 0.60, 0.38, 0.29

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -50,17 +50,19 @@ int fs_open(const char* pathname, int flags, int mode) {
 }
 
 size_t fs_read(int fd, void* buf, size_t len) {
-  assert(fd >= 3);
-  if (file_table[fd].open_offset > file_table[fd].size) {
-    len = 0;
+  if (file_table[fd].write) {
+    len = file_table[fd].write(buf, 0, len);
   }
-  else if (file_table[fd].open_offset + len >= file_table[fd].size) {
-    len = file_table[fd].size - file_table[fd].open_offset;
+  else {
+    if (file_table[fd].open_offset > file_table[fd].size)
+      len = 0;
+    else if (file_table[fd].open_offset + len >= file_table[fd].size)
+      len = file_table[fd].size - file_table[fd].open_offset;
+    
+    size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
+    ramdisk_read(buf, offset, len);
+    file_table[fd].open_offset += len;
   }
-
-  size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
-  ramdisk_read(buf, offset, len);
-  file_table[fd].open_offset += len;
   return len;
 }
 
