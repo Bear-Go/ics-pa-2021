4047773a3f800f55a9f3414a767bd57cef57d79e
tracer-ics2021 <tracer@njuics.org>
2022-01-10 21:19:44 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  21:19:44 up  8:22,  1 user,  load average: 0.11, 0.16, 0.15

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -49,10 +49,13 @@ int fs_open(const char* pathname, int flags, int mode) {
 }
 
 size_t fs_read(int fd, void* buf, size_t len) {
-  // assert(file_table[fd].open_offset <= file_table[fd].size);
   assert(fd >= 3);
-  if (file_table[fd].open_offset + len > file_table[fd].size) 
+  if (file_table[fd].open_offset > file_table[fd].size) {
+    len = 0;
+  }
+  else if (file_table[fd].open_offset + len >= file_table[fd].size) {
     len = file_table[fd].size - file_table[fd].open_offset;
+  }
   size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
   ramdisk_read(buf, offset, len);
   return len;
