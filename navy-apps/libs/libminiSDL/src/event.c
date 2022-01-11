cd80d475e91b1129382cb0cff54ac9c0dbca44e5
tracer-ics2021 <tracer@njuics.org>
2022-01-12 00:18:02 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  00:18:02 up 1 day,  2:27,  1 user,  load average: 1.11, 0.65, 0.44

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -20,13 +20,31 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  printf("sdl_pushevent\n");
-  assert(0);
+  char buf[32];
+  buf[0] = '\0';
+  int keycode = 0;
+  if (!NDL_PollEvent(buf, sizeof(buf))) {
+    ev->key.keysym.sym = SDLK_NONE;
+    ev->type = SDL_USEREVENT;
+    return 0;
+  }
+
+  char name[16];
+  sscanf(buf + 3, "%s", name);
+  for (int i = 0; i < key_num; ++ i)
+    if (strcmp(keyname[i], name) == 0) {
+      keycode = i;
+      break;
+    }
+  ev->type = buf[1] == 'u' ? SDL_KEYUP : SDL_KEYDOWN;
+  ev->key.keysym.sym = keycode;
+  keystate[keycode] == buf[1] == 'u' ? 0 : 1;
+  assert(keycode >= 1 && keycode < key_num);
   return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  char buf[128];
+  char buf[32];
   buf[0] = '\0';
   int keycode = 0;
   while (!NDL_PollEvent(buf, sizeof(buf)));
