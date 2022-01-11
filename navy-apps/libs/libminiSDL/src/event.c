95ea68ada2a95b9f14781127fcd9dadbb216978a
tracer-ics2021 <tracer@njuics.org>
2022-01-11 23:40:34 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  23:40:34 up 1 day,  1:50,  1 user,  load average: 0.19, 0.33, 0.39

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -13,46 +13,44 @@ static const char *keyname[] = {
 extern uint8_t* keystate;
 
 int SDL_PushEvent(SDL_Event *ev) {
+  printf("sdl_pushevent\n");
+  assert(0);
   return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  char buf[32];
-  buf[0] = '\0';
-  int keycode = 0;
-  if (NDL_PollEvent(buf,sizeof(buf)) == 0) {
-    ev->key.keysym.sym = SDLK_NONE;
-    ev->type = SDL_USEREVENT;
-    return 0;
-  }
-  else {
-    char* name = buf + 3;
-    for (int i = 0, cnt = 0; i < sizeof(keyname); i += strlen(keyname[cnt]), ++ cnt) {
-      bool is_find = !strncmp(keyname[cnt], buf, strlen(keyname[cnt]));
-      if (is_find) {
-        keycode = cnt;
-        break;
-      }
-    }
-
-    if (buf[1] == 'u') {
-      ev->key.keysym.sym = keycode;
-      ev->type = SDL_KEYUP;
-      keystate[keycode] = 0;
-    }
-    else if(buf[1] == 'd') {
-      ev->key.keysym.sym = keycode;
-      ev->type = SDL_KEYDOWN;
-      keystate[keycode] = 1;
-    }
-    else assert(0);
-    return 1;
-  }
+  printf("sdl_pushevent\n");
+  assert(0);
   return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+  char buf[32];
+  buf[0] = '\0';
+  int keycode = 0;
+  while (!NDL_PollEvent(buf, sizeof(buf)));
+
+  char* name = buf + 3;
+  for (int i = 0, cnt = 0; i < sizeof(keyname); i += strlen(keyname[cnt]), ++ cnt) {
+    bool is_find = !strncmp(keyname[cnt], buf, strlen(keyname[cnt]));
+    if (is_find) {
+      keycode = cnt;
+      break;
+    }
+  }
+
+  if (buf[1] == 'u') {
+    event->key.keysym.sym = keycode;
+    event->type = SDL_KEYUP;
+    keystate[keycode] = 0;
+  }
+  else if(buf[1] == 'd') {
+    event->key.keysym.sym = keycode;
+    event->type = SDL_KEYDOWN;
+    keystate[keycode] = 1;
+  }
+  else assert(0);
+  return 0;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
