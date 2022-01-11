6cf3b4945d7954d1524a2d7c0df7638b2d1c6036
tracer-ics2021 <tracer@njuics.org>
2022-01-11 16:45:23 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  16:45:23 up 19:26,  1 user,  load average: 0.47, 0.59, 0.49

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -27,10 +27,11 @@ size_t events_read(void *buf, size_t offset, size_t len) {
     return 0;
   }
   else{
-    if (ev.keydown)
-      sprintf((char*)buf,"kd %s\n",keyname[ev.keycode]);
-    else 
-      sprintf((char*)buf,"ku %s\n",keyname[ev.keycode]);
+    // if (ev.keydown)
+    //   sprintf((char*)buf,"kd %s\n",keyname[ev.keycode]);
+    // else 
+    //   sprintf((char*)buf,"ku %s\n",keyname[ev.keycode]);
+    sprintf((char*)buf, ev.keydown ? "kd %s\n" : "ku %s\n", keyname[ev.keycode]);
     return strlen((char*)buf);
   }
 }
