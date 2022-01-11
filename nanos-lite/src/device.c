71f053d24b4ae0f5fc0c17a56e09752ce9c27706
tracer-ics2021 <tracer@njuics.org>
2022-01-11 12:47:06 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  12:47:07 up 15:27,  1 user,  load average: 0.34, 0.72, 0.70

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,8 +28,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
     AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
 
     if (ev.keycode == AM_KEY_NONE) {
-          
-      return 0;
+      ev.keycode = 43;
     }
     else {
       panic("fuck here");
