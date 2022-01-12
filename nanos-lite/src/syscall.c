fb00443cf438c543986a4a5c7935e9572486e083
tracer-ics2021 <tracer@njuics.org>
2022-01-13 01:43:27 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  01:43:27 up 1 day, 18:58,  1 user,  load average: 0.95, 0.61, 0.60

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,16 @@
 #include <common.h>
 #include <sys/time.h>
 #include "syscall.h"
+#include <proc.h>
 
 int fs_open(const char* pathname, int flags, int mode);
 size_t fs_read(int fd, void* buf, size_t len);
 size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
+size_t sys_gettimeofday(struct timeval* tv, struct timezone* tz);
+size_t sys_execve(const char* filename, char* const argv[], char* const envp[]);
+void naive_uload(PCB *pcb, const char *filename);
 
 void do_syscall(Context *c) {
 
@@ -41,16 +45,28 @@ void do_syscall(Context *c) {
     case SYS_brk:
       c->GPRx = 0;
       break;
-    case SYS_gettimeofday: {
-      if ((void*)a[1] != NULL) {
-        struct timeval* tv = (struct timeval*)a[1]; 
-        (*tv).tv_usec = io_read(AM_TIMER_UPTIME).us % 1000000;
-        (*tv).tv_sec = io_read(AM_TIMER_UPTIME).us / 1000000;
-        c->GPRx = 0;
-      }
-      else c->GPRx = -1;
+    case SYS_execve:
+      sys_execve((char*)a[1], (char**)a[2], (char**)a[3]);
       break;
-    }
+    case SYS_gettimeofday: 
+      c->GPRx = sys_gettimeofday((struct timeval *)a[1], (struct timezone *)a[2]);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+}
+
+size_t sys_gettimeofday(struct timeval* tv, struct timezone* tz) {
+  if (tv != NULL) {
+    tv->tv_usec = io_read(AM_TIMER_UPTIME).us % 1000000;
+    tv->tv_sec = io_read(AM_TIMER_UPTIME).us / 1000000;
+  }
+  if (tz != NULL) {
+    panic("timezone unsupport");
+  }
+  return 0;
+}
+
+size_t sys_execve(const char* filename, char* const argv[], char* const envp[]) {
+  naive_uload(NULL, filename);
+  return -1;
 }
--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -5,10 +5,11 @@ int main() {
     int sec = 1;
     NDL_Init(0);
     while (1) {
+        if (sec == 60) break;
         while (NDL_GetTicks() / 1000 < sec) ;
         printf("Now, time is %d * half_sec\n", sec);
         sec ++;
     }
-
+    printf("1 min finished\ntimer-test passed\n");
     return 0;
 }
