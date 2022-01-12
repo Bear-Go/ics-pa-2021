24891b820446400bc457c292963a22a67f03c0f6
tracer-ics2021 <tracer@njuics.org>
2022-01-12 16:24:39 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  16:24:39 up 1 day,  9:39,  1 user,  load average: 0.96, 0.64, 0.50

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,11 +2,12 @@
 #include <NDL.h>
 
 int main() {
-    int half_sec = 1;
+    int sec = 1;
+    NDL_Init(0);
     while (1) {
-        while (NDL_GetTicks() / 500 < half_sec) ;
-        printf("Now, time is %d * half_sec\n", half_sec);
-        half_sec ++;
+        while (NDL_GetTicks() / 1000 < sec) ;
+        printf("Now, time is %d * half_sec\n", sec);
+        sec ++;
     }
 
     return 0;
