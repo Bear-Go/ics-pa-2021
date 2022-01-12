9fc1077e8288c7c91d5f3e461b1f9a8e433db561
tracer-ics2021 <tracer@njuics.org>
2022-01-12 14:50:20 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  14:50:20 up 1 day,  8:04,  1 user,  load average: 0.86, 0.57, 0.33

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -11,7 +11,6 @@ static const char *keyname[] = {
 };
 
 static int  key_num = sizeof(keyname) / sizeof(keyname[0]);
-extern uint8_t* keystate;
 
 int SDL_PushEvent(SDL_Event *ev) {
   printf("sdl_pushevent\n");
@@ -38,7 +37,6 @@ int SDL_PollEvent(SDL_Event *ev) {
     }
   ev->type = buf[1] == 'u' ? SDL_KEYUP : SDL_KEYDOWN;
   ev->key.keysym.sym = keycode;
-  keystate[keycode] == buf[1] == 'u' ? 0 : 1;
   assert(keycode >= 1 && keycode < key_num);
   return 1;
 }
@@ -58,7 +56,6 @@ int SDL_WaitEvent(SDL_Event *event) {
     }
   event->type = buf[1] == 'u' ? SDL_KEYUP : SDL_KEYDOWN;
   event->key.keysym.sym = keycode;
-  keystate[keycode] == buf[1] == 'u' ? 0 : 1;
   assert(keycode >= 1 && keycode < key_num);
   return 1;
 }
