760c6528832e0ee459446ea818cb4709f4180f9b
tracer-ics2021 <tracer@njuics.org>
2022-01-13 16:03:43 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  16:03:43 up 2 days,  1:48,  1 user,  load average: 0.51, 0.48, 0.37

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,9 +5,14 @@ extern void do_syscall(Context*);
 Context* schedule(Context *prev);
 
 static Context* do_event(Event e, Context* c) {
-  
+
   switch (e.event) {
-    case EVENT_YIELD: Log("event: yield"); return schedule(c); panic("should not reach here");
+    case EVENT_YIELD: {
+      Log("event: yield"); 
+      Context* ret = schedule(c);
+      return ret;
+      panic("should not reach here");
+    }
     case EVENT_SYSCALL: do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
