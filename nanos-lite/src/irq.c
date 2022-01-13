ff5363fcdb5752e769d38808bb10884f0b576442
tracer-ics2021 <tracer@njuics.org>
2022-01-13 16:04:34 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  16:04:34 up 2 days,  1:49,  1 user,  load average: 0.55, 0.53, 0.39

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -10,6 +10,7 @@ static Context* do_event(Event e, Context* c) {
     case EVENT_YIELD: {
       Log("event: yield"); 
       Context* ret = schedule(c);
+      assert(c != NULL);
       return ret;
       panic("should not reach here");
     }
