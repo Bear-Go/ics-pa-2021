9616ee89506ee492e46012d71409df2cbabf09d1
tracer-ics2021 <tracer@njuics.org>
2022-01-16 17:58:29 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  17:58:29 up 2 days,  5:16,  1 user,  load average: 0.77, 0.44, 0.33

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -60,6 +60,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context* c = (Context*)((uint8_t*)kstack.end - sizeof(Context) - sizeof(uintptr_t));
   c->mepc = (uintptr_t)entry;
+  c->GPR1 = 100;
   uintptr_t* argp = (uintptr_t*)((uint8_t*)kstack.start);
   *argp = (uintptr_t)arg;
   printf("arg = %p\n", arg);
