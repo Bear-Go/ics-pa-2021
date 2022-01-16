272ecba41a9b21a1b21cf15d384dd8423a5c2c46
tracer-ics2021 <tracer@njuics.org>
2022-01-16 17:52:50 +0800

    >  run 201870022    Static hostname: bear-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 99900caaa32341b195dfae24099e8af6            Boot ID: d2624b9d5ed348098fc4d10ec932dae0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  17:52:50 up 2 days,  5:10,  1 user,  load average: 0.53, 0.38, 0.30

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,7 +25,7 @@ void hello_fun(void *arg) {\
 }
 
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, (void*) 0xffffffff);
+  context_kload(&pcb[0], hello_fun, (void*) 23);
   context_kload(&pcb[1], hello_fun, (void*) 0xaaaaaaaa);
   switch_boot_pcb();
 
